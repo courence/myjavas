a2dcb0ce76e16a7110d9488527e1a6a7e1b24230
courence <courence@163.com>
2017-03-10 18:00:38 +0800

    11

---
+++ common/src/main/java/common/util/StdOutUtil.java
@@ -0,0 +1,12 @@
+package common.util;
+
+/**
+ * 标准文件输出
+ * @author jh
+ *
+ */
+public class StdOutUtil {
+	public static <T> void print(T context){
+		System.out.println(context);
+	}
+}
---
+++ myspring/src/test/java/test/spring/springinaction/common/springidol/Audience.java
@@ -0,0 +1,7 @@
+package test.spring.springinaction.common.springidol;
+
+public class Audience {
+	public void takeSeats(){
+		System.out
+	}
+}
